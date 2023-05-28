cb90c5433b518d74f1b4b6efa6f2473819909ddb
artexx <artexx@iMac-Egor.local>
2023-05-28 18:28:56 +0300

    feat: new homework6

---
+++ Desktop/lesson6/Program.cs
@@ -0,0 +1,48 @@
+﻿// 41
+/*
+Console.Write("Введите количество элементов массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[m];
+
+void mas(int m)
+{
+for (int i = 0; i < m; i++)
+{
+Console.WriteLine($"Введите {i+1} элемент массива ");
+Array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+}
+
+int kol(int[] Array)
+{
+int i=0;
+int sum = 0;
+while (i < Array.Length)
+{
+if(Array[i]>0)
+sum = sum + 1;
+i = i + 1;
+}
+return sum;
+}
+
+mas(m);
+Console.Write($"\n Чисел больше нуля: {kol(Array)}");
+*/
+
+//43
+
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
