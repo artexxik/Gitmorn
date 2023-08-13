5fbd3c09762010ddb6f072a83c805d2a34faf912
artexx <artexx@iMac-Egor.local>
2023-08-13 16:05:15 +0300

    feat: home work2

---
+++ Desktop/python/main.py
@@ -0,0 +1,33 @@
+#10
+
+#coins = [random.randint(0,1) for _ in range (9)]
+#print (coins)
+
+#count_0 = 0
+#count_1 = 0
+#for coin in coins:
+    #if coin ==0:
+         #count_0 +=1
+        
+    #else:
+         #count_1 += 1
+#if count_0 > count_1:
+    #print(" Перевернуть единички")    
+#else:
+    #print(" Перевернуть нолики")
+
+#12
+
+#sum = int(input("Введите сумму двух чисел: "))
+#mult = int(input("Введите произведение двух чисел: "))
+#print (sum, mult)
+#print ( round (mult / (sum - 2)))
+#print ( round (sum - (mult/(sum - 2))))
+
+#14
+
+#number = int (input("Введите предел: "))
+#i =1
+#while i< number:
+    #print (i,  end='  ')
+    #i*=2
